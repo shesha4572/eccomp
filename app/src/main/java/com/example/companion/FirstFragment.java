a10183c464bdf325294c23a2b74e54a539432672
shesha4572 <shesha4572@gmail.com>
2024-05-05 18:46:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/companion/FirstFragment.java
@@ -0,0 +1,51 @@
+package com.example.companion;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.companion.databinding.FragmentFirstBinding;
+
+import java.util.Objects;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonFirst.setOnClickListener(v -> {
+            Intent mid = requireActivity().getPackageManager().getLaunchIntentForPackage("com.example.myapplication");
+            assert mid != null;
+            String data = binding.editTextText.getText().toString() + "," + binding.editTextPhone.getText().toString() + "," + binding.editTextTextEmailAddress.getText().toString();
+            mid.putExtra("data" , data);
+            startActivity(mid);
+                }
+        );
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
