a34d5fb8fb8e061dcb42fb38e5ebd023c9046049
shesha4572 <shesha4572@gmail.com>
2024-05-20 16:59:51 +0530

    Working result callback

--- app/src/main/java/com/example/companion/FirstFragment.java
+++ app/src/main/java/com/example/companion/FirstFragment.java
@@ -1,11 +1,13 @@
 package com.example.companion;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
@@ -35,9 +37,10 @@ public class FirstFragment extends Fragment {
         binding.buttonFirst.setOnClickListener(v -> {
             Intent mid = requireActivity().getPackageManager().getLaunchIntentForPackage("com.example.myapplication");
             assert mid != null;
+            mid.setFlags(0);
             String data = binding.editTextText.getText().toString() + "," + binding.editTextPhone.getText().toString() + "," + binding.editTextTextEmailAddress.getText().toString();
             mid.putExtra("data" , data);
-            startActivity(mid);
+            ((MainActivity) requireActivity()).mStartForResult.launch(mid);
                 }
         );
     }
--- app/src/main/java/com/example/companion/MainActivity.java
+++ app/src/main/java/com/example/companion/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.companion;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -18,6 +24,7 @@ import com.example.companion.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -66,4 +73,23 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+    public ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+
+            result -> {
+                Log.i("SIGN" , String.valueOf(result.getResultCode()));
+                switch (result.getResultCode()) {
+                    case Activity.RESULT_OK:
+                        Intent intent = result.getData();
+                        String sign = intent.getStringExtra("sign");
+                        String signedData = intent.getStringExtra("data");
+                        Log.i("SIGN" , signedData);
+                        // Handle the Intent
+                        Toast.makeText(MainActivity.this, "Data signed with signature " + sign, Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(MainActivity.this, "Activity canceled", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            });
 }
